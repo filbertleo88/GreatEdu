906eacf7f5466fd2e1f7264f00e8ae4064f4294d
filbertleo88 <filbertleo88@gmail.com>
2024-05-27 14:26:31 +0700

    update

--- home.py
+++ home.py
@@ -31,7 +31,7 @@ def app():
     st.subheader('Dataset')
 
     # Membaca file CSV ke dalam DataFrame
-    default_file_path = "df_final.csv"
+    default_file_path = "df_daily_pollution_with_aqi.csv"
     df = load_data(default_file_path)
     df['Date'] = pd.to_datetime(df['Date'])
     df.set_index('Date', inplace=True)
